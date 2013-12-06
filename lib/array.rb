4ad63a7cd202b84ab7120b8ccce3ef04436d4f8a
astux7 <Asta.bevainyte@gmail.com>
2013-12-06 12:24:53 +0000

    working with interger array opperation

--- lib/array.rb
+++ lib/array.rb
@@ -1,3 +1,15 @@
 class Array
+  def implant(starter = nil)
+    starter = (starter == nil ? self.first : starter)
+    first_member = 0
+    return nil if !block_given?
+    self.each do |element|
+      starter +=  (yield first_member,element)
+      puts yield first_member, element
+    end
+   #return  memo == 0 
+   
+   return starter
+  end
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,6 +1,21 @@
 require_relative '../lib/array'
 
 describe Array do
-
-
+  let(:arr) {Array.new([11,2,3])}
+  it 'should return same count of elements' do
+    arr.implant
+    expect(arr.count).to eq (3)
+  end
+  it 'should return nil if block empty ' do
+    expect(arr.implant).to eq(nil)
+  end
+  it 'should return last element if given block like {|memo,num| num}' do
+    expect(arr.implant(0){|s,m| s += m}).to eq(16)
+  end
+   it 'should return last element if given block like {|memo,num| num}' do
+    expect(arr.implant(5){|s,m| s += m}).to eq(21)
+  end
+  it 'should return last element if given block like {|memo,num| num}' do
+    expect(arr.implant(0){|s,m| s -= m}).to eq(-16)
+  end
 end
