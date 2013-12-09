84c3ed72b9bb19e8e03c1003a296bb0aa2ef9a0c
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-12-09 16:46:11 +0000

    Feedback

--- lib/array.rb
+++ lib/array.rb
@@ -1,22 +1,36 @@
 class Array
   def implant(start = nil )
     return nil if !block_given?
+    # what's the point of setting starter to 0 if start.nil?
+    # if starter will be overwritten on line 9 anyway?
     starter = (start == nil ? 0 : start)
 
     if start.nil? 
       starter = self.first
       self.each do |element|
+        # this is dangerous. I wrote a test that fails because of this line
+        # Basically, it's unsafe to assume that the first element
+        # will be different from all others
         next if element == starter
-        starter = (yield starter,element)
-        puts yield starter, element
+        starter = yield starter, element
+        # don't put puts() in the code, otherwise your code
+        # will be impossible to use in a real situation
+        # Also, it makes rspec output look bad   
+        # Finally, because you're calling the block twice,
+        # this may have unintended side-effects (what if a block maintains some internal state?)     
       end
     else
+      # there's a lot of repetition here.
+      # Essentially, the else block is the same as the if block
+      # What if you did something like
+      #    self.slice(from..-1).each
+      # to iterate and changed the from variable depending on
+      # whether you have an argument
       self.each do |element|
-        starter = (yield starter,element)
-        puts yield starter, element
+        starter = yield starter,element        
       end
     end
-    return starter
+    starter
   end
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,41 +2,65 @@ require_relative '../lib/array'
 
 describe Array do
   let(:arr) {Array.new([11,2,3])}
+  # variable names could me more expressive: strings? string_array?
   let(:arrString) {Array.new(["aa","bb","cc"])}
+
   it 'should return same count of elements' do
+    # this differs from the default inject() implementation
+    # It should throw an error if there is no block and no arguments
     arr.implant
+    # but this is valid, because whatever arr.implant does, it should not modify an original array
     expect(arr.count).to eq (3)
   end
   it 'should return nil if block empty ' do
+    # same here: real inject would raise an exception
     expect(arr.implant).to eq(nil)
   end
+
+  # all other tests make sense
+  # Try to leave a blank line between tests
+
   it 'should return sum if given block like {|s,n| s+=n} but has a start value 0' do
     expect(arr.implant(0){|s,m| s += m}).to eq(16)
   end
-   it 'should return sum if given block like {|s,n| s+=n} but has a start value 5 ' do
+
+  it 'should return sum if given block like {|s,n| s+=n} but has a start value 5 ' do
     expect(arr.implant(5){|s,m| s += m}).to eq(21)
   end
+
   it 'should return sub if given block like {|s,n| s-=n} but has a start value 0 ' do
     expect(arr.implant(0){|s,m| s -= m}).to eq(-16)
   end
+
   it 'should return sub if given block like {|s,n| s-=n} but hasnt a start value ' do
      expect(arr.implant{|s,m| s -= m}).to eq(6)
   end
-   it 'should return multi if given block like {|s,n| s*=n} but has a start value 1' do
+
+  it 'should return multi if given block like {|s,n| s*=n} but has a start value 1' do
      expect(arr.implant(1){|s,m| s *= m}).to eq(66)
   end
+
   it 'should return multi if given block like {|s,n| s*=n} but has a start value 3' do
      expect(arr.implant(3){|s,m| s *= m}).to eq(198)
   end
+
   it 'should return multi if given block like {|s,n| s*=n} but hasnt a start value ' do
      expect(arr.implant{|s,m| s *= m}).to eq(66)
   end
+
   it 'should return div if given block like {|s,n| s/=n} but hasnt a start value ' do
      expect(arr.implant(132){|s,m| s /= m}).to eq(2)
   end
+
   it 'adding string without first val' do
      expect(arrString.implant{|s,m| s += m}).to eq("aabbcc")
   end
+
+  # This fails because of the error in the implementation
+  it 'adding string without first val' do
+     expect(['a', 'a', 'a'].implant{|s,m| s += m}).to eq("aaa")
+  end
+
   it 'adding string with first val' do
      expect(arrString.implant("bb"){|s,m| s += m}).to eq("bbaabbcc")
   end
